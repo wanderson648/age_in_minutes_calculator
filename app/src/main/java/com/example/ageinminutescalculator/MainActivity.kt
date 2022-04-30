265588f69e9fe4f97091b6bb26620e4af275f970
wanderson648 <testepedroca@gmail.com>
2022-04-29 21:20:06 -0300

    selecionando data e mostrando na tela

--- app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.ageinminutescalculator
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate: TextView? = null
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button =  findViewById(R.id.btn_date_picker)
+        tvSelectedDate = findViewById(R.id.tv_selected_date)
+
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun clickDatePicker() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this, { view, selectedYear, selectedMonth, selectedDay ->
+            val selectedDate = "$selectedDay/${selectedMonth+1}/$selectedYear"
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            val theDate = sdf.parse(selectedDate)
+
+            tvSelectedDate?.text = selectedDate
+
+            Toast.makeText(this,
+                "$theDate",
+                Toast.LENGTH_SHORT).show()
+        },
+            year,
+            month,
+            day
+        ).show()
+
     }
 }
